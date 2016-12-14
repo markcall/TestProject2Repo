70d2f7569d6982e0e9948bb9e681dbdc2d2ad630
Marks Virtual machine <IEUser@10.0.2.15>
2016-12-14 03:15:15 -0800

    Change 2

--- TestProject2/src/com/mark/Test1.java
+++ TestProject2/src/com/mark/Test1.java
@@ -5,6 +5,10 @@ public class Test1 {
 	public static void main(String[] args){
 		System.out.println("Some Testing!");
 		System.out.println("1111111");
+		
+		System.out.println("2222222222");
+		
+		System.out.println("33333333333");
 	}
 
 }
