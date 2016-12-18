01e9394b2cf105044f16633115ca6bc1a2351ccd
Mark Pergamenshik <markp@callup.net>
2016-12-18 16:58:14 +0200

    Adding some data here also

--- TestProject2/src/com/mark/Tes2.java
+++ TestProject2/src/com/mark/Tes2.java
@@ -5,6 +5,7 @@ public class Tes2 {
 	public static void main(String[] args) {
 		System.out.println("This is a local update");
 		System.out.println("This is remote update number 3 ");
+		System.out.println("Adding some new dat here also");
 	}
 
 }
