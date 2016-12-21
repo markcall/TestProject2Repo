1f6ddd15725817d6820d139aa1bb78ae6a88a88c
Marks Virtual machine <IEUser@10.0.2.15>
2016-12-21 03:12:11 -0800

    Test5 change 2

--- TestProject2/src/com/mark/Test5.java
+++ TestProject2/src/com/mark/Test5.java
@@ -5,6 +5,11 @@ public class Test5 {
 	public void bar1(){
 		System.out.println("bar1");
 	}
+	
+	public void bar2(){
+		System.out.println("bar2");
+	}
+	
 	public void foo1(){
 		System.out.println("foo1");
 	}
